92a94addd0c504c26818215b9dc10aecc962d4b6
Mirel Mitache <mirel.mitache@avira.com>
2014-11-07 16:03:12 +0200

    Replaced array() with [] in a few places

--- php/config/config.inc.php
+++ php/config/config.inc.php
@@ -5,22 +5,24 @@ if (file_exists($dbConfigFile = dirname(dirname(dirname(__DIR__))) . DIRECTORY_S
 }
 
 
-return array(
-    'mpf\\interfaces\\LogAwareObjectInterface' => array(
-        'loggers' => array('mpf\\loggers\\InlineWebLogger')
-    ),
-    'mpf\\datasources\\sql\\PDOConnection' => array(
+return [
+    'mpf\\interfaces\\LogAwareObjectInterface' => [
+        'loggers' => ['mpf\\loggers\\InlineWebLogger']
+    ],
+    'mpf\\datasources\\sql\\PDOConnection' => [
         'dns' => 'mysql:dbname=demoapp;host=' . (defined('DB_HOST') ? DB_HOST : 'localhost'),
         'username' => defined('DB_USER') ? DB_USER : 'root',
         'password' => defined('DB_PASS') ? DB_PASS : ''
-    ),
-    'mpf\\interfaces\\TranslatableObjectInterface' => array(
+    ],
+    'mpf\\interfaces\\TranslatableObjectInterface' => [
         'translator' => '\\mpf\\translators\\ArrayFile'
-    ),
-    'mpf\\web\\AssetsPublisher' => array(
+    ],
+    'mpf\\web\\AssetsPublisher' => [
         'developmentMode' => true // change it to true when working on widgets or any other classes that publish assets that are changed during development
-    ),
-    'mpf\\base\\App' => array(
+    ],
+    'mpf\\base\\App' => [
+        'shortName' => 'app',
+        'title' => 'BASIC - App Template'
 //        'cacheEngineClass' => '\\mpf\\datasources\\redis\\Cache'
-    )
-);
+    ]
+];
--- php/config/web.inc.php
+++ php/config/web.inc.php
@@ -3,35 +3,31 @@
 $main = include(__DIR__ . DIRECTORY_SEPARATOR . 'config.inc.php');
 
 $main['mpf\\interfaces\\LogAwareObjectInterface'] = isset($main['mpf\\interfaces\\LogAwareObjectInterface']) ? $main['mpf\\interfaces\\LogAwareObjectInterface'] : array();
-$main['mpf\\interfaces\\LogAwareObjectInterface']['loggers'] = array('mpf\\loggers\\DevLogger');
+$main['mpf\\interfaces\\LogAwareObjectInterface']['loggers'] = ['mpf\\loggers\\DevLogger'];
 
-$main['mpf\\WebApp'] = array(
-    'title' => 'Tests'
-);
-
-$main['mpf\\web\\Cookie'] = array(
+$main['mpf\\web\\Cookie'] = [
     'salt' => '122#!@#3424#@$#25543'
-);
-$main['mpf\\web\\request\\HTML'] = array(
+];
+$main['mpf\\web\\request\\HTML'] = [
     'SEO' => true,
-    'urlRoutes' => array(
+    'urlRoutes' => [
         '(?<language>[a-z]{2})\/(?<controller>[a-zA-Z0-9]+)\/(?<action>[a-zA-Z0-9_\-]+)\/(?<id>[0-9]+)', // language & controller & action  & id
         '(?<controller>[a-zA-Z0-9]+)\/(?<action>[a-zA-Z0-9_\-]+)\/(?<id>[0-9]+)', // controller & action & id
         '(?<language>[a-z]{2})\/(?<controller>[a-zA-Z0-9]+)\/(?<action>[a-zA-Z0-9_\-]+)', // language & controller & action
-        '(?<language>[a-z]{2})\/(?<controller>[a-zA-Z0-9]+)\/(?<id>[0-9]+)' => array('action' => 'view'), // language & controller & view id
-        '(?<controller>[a-zA-Z0-9]+)\/(?<id>[0-9]+)' => array('action' => 'view'), // controller & view id
+        '(?<language>[a-z]{2})\/(?<controller>[a-zA-Z0-9]+)\/(?<id>[0-9]+)' => ['action' => 'view'], // language & controller & view id
+        '(?<controller>[a-zA-Z0-9]+)\/(?<id>[0-9]+)' => ['action' => 'view'], // controller & view id
         '(?<language>[a-z]{2})\/(?<controller>[a-zA-Z0-9]+)', // language & controller
         '(?<controller>[a-zA-Z0-9]+)\/(?<action>[a-zA-Z0-9_\-]+)', // controller & action
         '(?<controller>[a-zA-Z0-9]+)' // controller
-    ),
-    'modules' => array(
-        'admin' => array(
-            'mpf\\WebApp' => array(
-                'title' => $main['mpf\\WebApp']['title'] . ' - Admin'
-            )
-        )
-    ),
+    ],
+    'modules' => [
+        'admin' => [
+            'mpf\\WebApp' => [
+                'title' => $main['mpf\\base\\App']['title'] . ' - Admin'
+            ]
+        ]
+    ],
     'csrfSalt' => 'D$F#$dx32x43'
-);
+];
 
 return $main;
