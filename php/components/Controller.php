920c851733895f7e64e02e474abb6d9f05cee979
Mirel Mitache <mirel.mitache@avira.com>
2015-12-08 13:43:22 +0200

    Add markdown support

--- php/components/Controller.php
+++ php/components/Controller.php
@@ -10,12 +10,22 @@ namespace app\components;
 
 
 //use app\components\htmltools\Page;
+use mpf\widgets\form\fields\Markdown;
 
 class Controller extends \mpf\web\Controller {
 
     protected function init($config = array()) {
         //Page::get()->getTranslator()->setLanguage('ro');
         return parent::init($config);
+    }
+
+    protected function beforeAction($actionName) {
+        if (isset($_POST['MarkdownPreview'])){ // only needed when using markdown
+            echo Markdown::processText(htmlentities($_POST['text']));
+            return false;
+        }
+
+        return parent::beforeAction($actionName);
     }
 
     protected function afterAction($actionName, &$result) {
